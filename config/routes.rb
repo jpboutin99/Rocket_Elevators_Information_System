f70c158b1944e2e3151db81a5da345a121239a8b
jpboutin99 <42749424+jpboutin99@users.noreply.github.com>
2018-10-03 09:21:58 -0400

    wednesday

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   get '/residential' =>'home#residential'
+  get '/commercial' =>'home#commercial'
   
   root 'home#index'
 end
