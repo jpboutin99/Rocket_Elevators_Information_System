ee1b3793389ce4a5e021919a339ad9feb2395e17
jpboutin99 <42749424+jpboutin99@users.noreply.github.com>
2018-10-05 15:49:16 -0400

    15:50

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Rails.application.routes.draw do
   get '/residential' =>'home#residential'
   get '/commercial' =>'home#commercial'
   get '/quote' =>'quote#quote'
+  get '/index-RocketElevators.html' =>'home#index'
+  get '/index-rocketElevators-residential.html' => 'home#residential'
+  get '/index-rocketElevators-commercial.html' => 'home#commercial'
+  get '/index-Quote.html' => 'quote#quote'
   post '/new_quote' =>'quote#new_quote'
   devise_for :users, :controllers => { registrations: 'registrations' }
   root 'home#index'
