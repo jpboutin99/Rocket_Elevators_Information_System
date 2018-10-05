03df9eb3505690533eca3aaf0a1d20da98898b68
jpboutin99 <42749424+jpboutin99@users.noreply.github.com>
2018-10-05 14:16:36 -0400

    vendredi 14:20

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -9,4 +9,6 @@ class RegistrationsController < Devise::RegistrationsController
   def account_update_params
     params.require(:user).permit(:first_name, :last_name, :title, :email, :password, :password_confirmation, :current_password)
   end
+
+
 end
