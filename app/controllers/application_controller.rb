95a61055cd32a8303f3236d1ad814eb6f553993d
jpboutin99 <42749424+jpboutin99@users.noreply.github.com>
2018-10-05 17:40:57 -0400

    final

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery prepend: true  
+  protect_from_forgery prepend: true
   
   protected
     def after_sign_in_path_for(resource)
