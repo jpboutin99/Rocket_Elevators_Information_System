f7b32cf8acee5d20a5d138e96db12f3a7e7ae0a0
jpboutin99 <42749424+jpboutin99@users.noreply.github.com>
2018-10-11 14:34:12 -0400

    creationPostgresql

--- app/models/battery.rb
+++ app/models/battery.rb
@@ -3,4 +3,9 @@ class Battery < ApplicationRecord
   belongs_to :Rocket_Elevator_Manager, :class_name => 'Employee', :foreign_key => 'employee_id'
 
   has_many :column
+  rails_admin do
+    edit do
+      exclude_fields :column
+    end
+  end
 end
--- app/models/building_detail.rb
+++ app/models/building_detail.rb
@@ -1,8 +1,6 @@
 class BuildingDetail < ApplicationRecord
   belongs_to :building
-  def information_key_enum
-        [['Number of Floors'],['Building Type'],['Building Architecture'],['Max Occupency'],['Built Year']]
-  end
+  
   def name
     "#{self.information_key}: #{self.value}"
   end
